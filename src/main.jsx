9631bad8365a17c0f462e165dcb97b2b190d2d6b
Sebastián <00373119@uca.edu.sv>
2022-11-30 23:16:02 -0600

    Change env and main.jsx files

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import {BrowserRouter} from 'react-router-dom';
-import {ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios';
-import {ConfigProvider} from './contexts/ConfigContexts';
-import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
-import {UserContextProvider} from './contexts/UserContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { ConfigProvider } from "./contexts/ConfigContexts";
+import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
+import { UserContextProvider } from "./contexts/UserContext";
 
-axios.defaults.baseURL=import.meta.env.VITE_APIENDPOINT || "http://localhost:3500/api";
+axios.defaults.baseURL =
+  import.meta.env.VITE_APIENDPOINT || "eleven-page-api.up.railway.app/api/";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <BrowserRouter>
-     <ConfigProvider>
-     <UserContextProvider>
-      <App />
-      <ToastContainer theme='dark' position='bottom-right'/>
-       <LoadingSpinner/>
-       </UserContextProvider>
-     </ConfigProvider>
-   </BrowserRouter>
-
-
+    <BrowserRouter>
+      <ConfigProvider>
+        <UserContextProvider>
+          <App />
+          <ToastContainer theme="dark" position="bottom-right" />
+          <LoadingSpinner />
+        </UserContextProvider>
+      </ConfigProvider>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
